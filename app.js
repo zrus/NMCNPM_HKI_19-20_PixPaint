72c52cfd634672958f17acf6e7a6b2a411ca5b8d
Dilvika <tuongn09@gmail.com>
2020-01-04 21:52:12 +0700

    Convert to handlebars

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const app = express();
+
+let expressHbs = require('express-handlebars');
+
+app.use(express.static(__dirname + '/assets'));
+
+let hbs = expressHbs.create({
+    extname: 'hbs',
+    defaultLayout: 'layouts',
+    layoutsDir: __dirname + '/views/layouts/'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('pixpaint');
+});
+  
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), () => {
+    console.log('Server is running');
+});
+
+
+
