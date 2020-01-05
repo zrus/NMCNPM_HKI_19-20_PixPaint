8d70aa9241fbe31346c5f001f86567d471229e08
Tri Nguyen <nguyenduongtri0503@gmail.com>
2020-01-05 13:30:16 +0700

    log in + sign up + log out + session + set up database (only user)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 let expressHbs = require('express-handlebars');
+let bodyParse = require('body-parser');
 
 app.use(express.static(__dirname + '/assets'));
 
@@ -14,10 +15,42 @@ let hbs = expressHbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+//Body Parser Middleware
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+
+//Cookie Parser
+let cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//Session
+let session = require('express-session');
+app.use(session({
+    cookie: { httpOnly: true, maxAge: null },
+    secret: 'S3cret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+    res.locals.username = req.session.user ? req.session.user.username : '';
+    res.locals.isLoggedIn = req.session.user ? true : false;
+    next();
+});
+
+app.use('/users', require('./routes/userRouter'));
+
 app.get('/', (req, res) => {
-    res.render('pixpaint');
+    res.render('index');
 });
   
+app.get('/sync', (req, res) => {
+    let models = require('./models');
+    models.sequelize.sync()
+    .then(() => {
+        res.send('database sync completed!')
+    });
+});
 
 const PORT = 5000;
 app.set('port', process.env.PORT || PORT);
