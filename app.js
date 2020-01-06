eb777db2294fe73b1b0b5f94f65ecff8a269b999
Dilvika <tuongn09@gmail.com>
2020-01-06 12:28:09 +0700

    Create grid for canvas

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/userRouter'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('pixpaint');
 });
   
 app.get('/sync', (req, res) => {
