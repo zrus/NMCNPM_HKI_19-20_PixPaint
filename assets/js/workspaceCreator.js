3c7351ce6ec13da85c1512e6ccf41a1f47e507ac
Tri Nguyen <nguyenduongtri0503@gmail.com>
2020-01-09 12:32:52 +0700

    fix line

--- assets/js/workspaceCreator.js
+++ assets/js/workspaceCreator.js
@@ -61,8 +61,8 @@ class PictureCanvas {
   }
   syncState(picture) {
     if (this.picture == picture) return;
-    drawPicture(picture, this.dom, scale, this.picture);
     this.picture = picture;
+    drawPicture(this.picture, this.dom, scale, null);
   }
 }
 
